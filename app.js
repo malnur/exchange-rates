adee402f119fb6502a5ee5f23efcf0f6f07460c1
Abdul-MAlik MArkabayev <amarkabayev@beeline.kz>
2024-01-18 02:01:00 +0600

    Добавить обмен курсами

--- app.js
+++ app.js
@@ -17,13 +17,25 @@ function newElement(tag, attrs = null, text = null) {
 const inputAmount = document.getElementById("amount");
 const selectFrom = document.getElementById("from-currency");
 const selectTo = document.getElementById("to-currency");
+const spanFrom = document.getElementById("from-result");
+const spanTo = document.getElementById("to-result");
 let codes = [];
 
 async function loadAPICodes() {
-  const resCodes = await fetch(
-    `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`
-  );
-  const jsonCodes = await resCodes.json();
+  let jsonCodes = {};
+  try {
+    const resCodes = await fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`
+    );
+    jsonCodes = await resCodes.json();
+  } catch (error) {
+    showError("Проблема со связью");
+    return;
+  }
+  if (jsonCodes.result == "error") {
+    showError("API вернул ошибку");
+    return;
+  }
 
   codes = jsonCodes.supported_codes;
   codes.sort((a, b) => {
@@ -55,15 +67,27 @@ async function loadAPICodes() {
   loadRate("USD", "KZT", divRates);
   loadRate("EUR", "KZT", divRates);
 
+  inputAmount.value = 1;
   selectFrom.value = "USD";
   selectTo.value = "KZT";
+  loadRateAmount();
 }
 
 async function loadRate(curr1, curr2, parent) {
-  const resRate = await fetch(
-    `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${curr1}/${curr2}`
-  );
-  const jsonRate = await resRate.json();
+  let jsonRate = {};
+  try {
+    const resRate = await fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${curr1}/${curr2}`
+    );
+    jsonRate = await resRate.json();
+  } catch (error) {
+    showError("Проблема со связью");
+    return;
+  }
+  if (!jsonRate.result == "error") {
+    showError("API вернул ошибку");
+    return;
+  }
 
   const divRow = newElement("div");
   divRow.className = "row";
@@ -91,6 +115,61 @@ function getCurrencyName(currencyCode) {
   return result;
 }
 
-loadAPICodes();
+inputAmount.addEventListener("input", () => {
+  loadRateAmount();
+});
+selectFrom.addEventListener("change", () => {
+  loadRateAmount();
+});
+selectTo.addEventListener("change", () => {
+  loadRateAmount();
+});
 
-inputAmount.value = 1;
+async function loadRateAmount() {
+  hideError();
+
+  const amount = inputAmount.value;
+  if (!amount) {
+    return;
+  }
+  const curr1 = selectFrom.value;
+  const curr2 = selectTo.value;
+
+  let jsonRate = {};
+  try {
+    const resRate = await fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${curr1}/${curr2}/${amount}`
+    );
+    jsonRate = await resRate.json();
+  } catch (error) {
+    showError("Проблема со связью");
+    return;
+  }
+  if (!jsonRate.result == "error") {
+    showError("API вернул ошибку");
+    return;
+  }
+
+  spanFrom.textContent = `${amount} (${curr1})`;
+  spanTo.textContent = `${jsonRate.conversion_result} (${curr2})`;
+}
+
+function showError(message) {
+  const divResult = document.getElementById("result");
+  divResult.style.display = "none";
+
+  const divError = document.getElementById("error");
+  divError.textContent = message;
+  divError.style.display = "block";
+}
+
+function hideError() {
+  const divResult = document.getElementById("result");
+  divResult.style.display = "block";
+
+  const divError = document.getElementById("error");
+  divError.textContent = "";
+  divError.style.display = "none";
+}
+
+loadAPICodes();
